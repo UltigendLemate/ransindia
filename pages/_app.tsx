d3fe55bde842e38fb5c8ad26b870e5fe6d5f7a13
Chaitanya Anand <chaitanya.anand@gmail.com>
2023-12-21 23:21:42 +0530

    error fix?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+// import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { fontSans, fontMono } from "@/config/fonts";
 import { useRouter } from "next/router";
 import "@/styles/globals.css";
@@ -11,9 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <NextUIProvider navigate={router.push}>
-      <NextThemesProvider>
+
         <Component {...pageProps} />
-      </NextThemesProvider>
+ 
     </NextUIProvider>
   );
 }
